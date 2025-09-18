dd0b50fc5dcf3a230c4ba31a82f1b8061c71da33
Spawn <spwn02.dev@gmail.com>
2025-09-18 13:48:15 +0200

    Added Nyx key and mouse codes.

--- Nyx/src/Nyx.h
+++ Nyx/src/Nyx.h
@@ -1,10 +1,14 @@
 #pragma once
 
 // For use by Nyx applications
+
 #include "Nyx/Application.h"
-#include "Nyx/Input.h"
 #include "Nyx/Layer.h"
 #include "Nyx/Log.h"
+
+#include "Nyx/Input.h"
+#include "Nyx/KeyCodes.h"
+#include "Nyx/MouseButtonCodes.h"
 
 #include "Nyx/ImGui/ImGuiLayer.h"
 
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -9,12 +9,19 @@ public:
 
 	void onUpdate() override
 	{
-		// NYX_INFO("ExampleLayer::Update");
+		if (Nyx::Input::isKeyPressed(NYX_KEY_TAB))
+			NYX_TRACE("Tab key is pressed!");
 	}
 
 	void onEvent(Nyx::Event& event) override
 	{
-		// NYX_INFO("{0}", event.toString());
+		if (event.getEventType() == Nyx::EventType::KeyPressed)
+		{
+			Nyx::KeyPressedEvent& e = (Nyx::KeyPressedEvent&)event;
+			if (e.getKeyCode() == NYX_KEY_TAB)
+				NYX_TRACE("Tab key is pressed!");
+			NYX_TRACE("{0}", (char)e.getKeyCode());
+		}
 	}
 };
 
