98c30c22c3d29bc482326e67bda48f1e2085c471
Spawn <spwn02.dev@gmail.com>
2025-09-18 13:55:03 +0200

    Added Nyx key and mouse codes source files.

---
+++ Nyx/src/Nyx/KeyCodes.h
@@ -0,0 +1,125 @@
+#pragma once
+
+// From glfw3.h
+#define NYX_KEY_SPACE              32
+#define NYX_KEY_APOSTROPHE         39  /* ' */
+#define NYX_KEY_COMMA              44  /* , */
+#define NYX_KEY_MINUS              45  /* - */
+#define NYX_KEY_PERIOD             46  /* . */
+#define NYX_KEY_SLASH              47  /* / */
+#define NYX_KEY_0                  48
+#define NYX_KEY_1                  49
+#define NYX_KEY_2                  50
+#define NYX_KEY_3                  51
+#define NYX_KEY_4                  52
+#define NYX_KEY_5                  53
+#define NYX_KEY_6                  54
+#define NYX_KEY_7                  55
+#define NYX_KEY_8                  56
+#define NYX_KEY_9                  57
+#define NYX_KEY_SEMICOLON          59  /* ; */
+#define NYX_KEY_EQUAL              61  /* = */
+#define NYX_KEY_A                  65
+#define NYX_KEY_B                  66
+#define NYX_KEY_C                  67
+#define NYX_KEY_D                  68
+#define NYX_KEY_E                  69
+#define NYX_KEY_F                  70
+#define NYX_KEY_G                  71
+#define NYX_KEY_H                  72
+#define NYX_KEY_I                  73
+#define NYX_KEY_J                  74
+#define NYX_KEY_K                  75
+#define NYX_KEY_L                  76
+#define NYX_KEY_M                  77
+#define NYX_KEY_N                  78
+#define NYX_KEY_O                  79
+#define NYX_KEY_P                  80
+#define NYX_KEY_Q                  81
+#define NYX_KEY_R                  82
+#define NYX_KEY_S                  83
+#define NYX_KEY_T                  84
+#define NYX_KEY_U                  85
+#define NYX_KEY_V                  86
+#define NYX_KEY_W                  87
+#define NYX_KEY_X                  88
+#define NYX_KEY_Y                  89
+#define NYX_KEY_Z                  90
+#define NYX_KEY_LEFT_BRACKET       91  /* [ */
+#define NYX_KEY_BACKSLASH          92  /* \ */
+#define NYX_KEY_RIGHT_BRACKET      93  /* ] */
+#define NYX_KEY_GRAVE_ACCENT       96  /* ` */
+#define NYX_KEY_WORLD_1            161 /* non-US #1 */
+#define NYX_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define NYX_KEY_ESCAPE             256
+#define NYX_KEY_ENTER              257
+#define NYX_KEY_TAB                258
+#define NYX_KEY_BACKSPACE          259
+#define NYX_KEY_INSERT             260
+#define NYX_KEY_DELETE             261
+#define NYX_KEY_RIGHT              262
+#define NYX_KEY_LEFT               263
+#define NYX_KEY_DOWN               264
+#define NYX_KEY_UP                 265
+#define NYX_KEY_PAGE_UP            266
+#define NYX_KEY_PAGE_DOWN          267
+#define NYX_KEY_HOME               268
+#define NYX_KEY_END                269
+#define NYX_KEY_CAPS_LOCK          280
+#define NYX_KEY_SCROLL_LOCK        281
+#define NYX_KEY_NUM_LOCK           282
+#define NYX_KEY_PRINT_SCREEN       283
+#define NYX_KEY_PAUSE              284
+#define NYX_KEY_F1                 290
+#define NYX_KEY_F2                 291
+#define NYX_KEY_F3                 292
+#define NYX_KEY_F4                 293
+#define NYX_KEY_F5                 294
+#define NYX_KEY_F6                 295
+#define NYX_KEY_F7                 296
+#define NYX_KEY_F8                 297
+#define NYX_KEY_F9                 298
+#define NYX_KEY_F10                299
+#define NYX_KEY_F11                300
+#define NYX_KEY_F12                301
+#define NYX_KEY_F13                302
+#define NYX_KEY_F14                303
+#define NYX_KEY_F15                304
+#define NYX_KEY_F16                305
+#define NYX_KEY_F17                306
+#define NYX_KEY_F18                307
+#define NYX_KEY_F19                308
+#define NYX_KEY_F20                309
+#define NYX_KEY_F21                310
+#define NYX_KEY_F22                311
+#define NYX_KEY_F23                312
+#define NYX_KEY_F24                313
+#define NYX_KEY_F25                314
+#define NYX_KEY_KP_0               320
+#define NYX_KEY_KP_1               321
+#define NYX_KEY_KP_2               322
+#define NYX_KEY_KP_3               323
+#define NYX_KEY_KP_4               324
+#define NYX_KEY_KP_5               325
+#define NYX_KEY_KP_6               326
+#define NYX_KEY_KP_7               327
+#define NYX_KEY_KP_8               328
+#define NYX_KEY_KP_9               329
+#define NYX_KEY_KP_DECIMAL         330
+#define NYX_KEY_KP_DIVIDE          331
+#define NYX_KEY_KP_MULTIPLY        332
+#define NYX_KEY_KP_SUBTRACT        333
+#define NYX_KEY_KP_ADD             334
+#define NYX_KEY_KP_ENTER           335
+#define NYX_KEY_KP_EQUAL           336
+#define NYX_KEY_LEFT_SHIFT         340
+#define NYX_KEY_LEFT_CONTROL       341
+#define NYX_KEY_LEFT_ALT           342
+#define NYX_KEY_LEFT_SUPER         343
+#define NYX_KEY_RIGHT_SHIFT        344
+#define NYX_KEY_RIGHT_CONTROL      345
+#define NYX_KEY_RIGHT_ALT          346
+#define NYX_KEY_RIGHT_SUPER        347
+#define NYX_KEY_MENU               348
---
+++ Nyx/src/Nyx/MouseButtonCodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// From glfw3.h
+#define NYX_MOUSE_BUTTON_1         0
+#define NYX_MOUSE_BUTTON_2         1
+#define NYX_MOUSE_BUTTON_3         2
+#define NYX_MOUSE_BUTTON_4         3
+#define NYX_MOUSE_BUTTON_5         4
+#define NYX_MOUSE_BUTTON_6         5
+#define NYX_MOUSE_BUTTON_7         6
+#define NYX_MOUSE_BUTTON_8         7
+#define NYX_MOUSE_BUTTON_LAST      NYX_MOUSE_BUTTON_8
+#define NYX_MOUSE_BUTTON_LEFT      NYX_MOUSE_BUTTON_1
+#define NYX_MOUSE_BUTTON_RIGHT     NYX_MOUSE_BUTTON_2
+#define NYX_MOUSE_BUTTON_MIDDLE    NYX_MOUSE_BUTTON_3
